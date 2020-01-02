bddf75201f658ee9ef3cda1b82c3a418ba9b6ff6
Poicaz <1433478584@qq.com>
2020-01-02 20:50:31 +0800

    v2.0

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -11,6 +11,7 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
         System.out.println("this is bad guy");
+        System.out.println("this is bad guy");
     }
 
 }
